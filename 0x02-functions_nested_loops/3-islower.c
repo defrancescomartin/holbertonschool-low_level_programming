6c6b9596049f43b327f47624a7f9a1b6e1d5f0d0
MartinDeFrancesco <defrancescoma@gmail.com>
2022-02-07 07:35:58 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * _islower - Prints 1 if the letter is lowercase
+ * and if it is uppercase it prints 0
+ *
+ * Description: Same as above.
+ *
+ * Return: Always 0 (Sucess)
+ **/
+
+int _islower(int letra)
+{
+	if (letra >= 'a' && letra <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
