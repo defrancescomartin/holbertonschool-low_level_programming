51d493d304dd58a771e66ba20eafde41de26bb6a
MartinDeFrancesco <defrancescoma@gmail.com>
2022-02-07 19:06:28 -0800

    natural multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+nclude <stdio.h>
+
+/**
+ * main - list all natural numbers multiples of 3 & 5
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i, suma;
+
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			suma += i;
+		}
+
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
