1cd1b39a93827d87fd2886cf9230dbfa0b57a6d6
MartinDeFrancesco <defrancescoma@gmail.com>
2022-02-07 19:15:16 -0800

    fib 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+ * main - print the 50 Fib numbers starting 
+ * with 1 and 2
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int suma;
+	long int fst, snd;
+	int contador;
+
+	suma = 0;
+	fst = 0;
+	snd = 1;
+	count = 0;
+
+	while (contador < 49)
+	{
+		suma = fst + snd;
+		printf("%li, ", sum);
+		fst = snd;
+		snd = suma;
+
+		contador++;
+	}
+	suma = fst + snd;
+	printf("%li\n", suma);
+
+	return (0);
+}
