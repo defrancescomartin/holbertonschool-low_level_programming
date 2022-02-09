cc486155864e23d4b534c6dfd15c037445ce1e36
MartinDeFrancesco <defrancescoma@gmail.com>
2022-02-09 07:38:02 -0800

    print diagonal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * putchars - print recursively all cahrs from an int number.
+ * @n: spects intger as an argument
+ *
+ * Return: Void
+ */
+void putchars(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		putchars(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+}
+
+/**
  * more_numbers - Prints 0-14 numbers 10 times
  * Description: Same as above
  *
@@ -9,5 +31,5 @@
 
 void more_numbers(void)
 {
-
+	putchars(n)
 }
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a n length \ diagonal line
+ * @n: length of line
+ * Description: Same as above
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int line = 0;
+	int space;
+
+	if (n > 0)
+	{
+		while (line < n)
+			for (space = 0; space < line; space++)
+			{
+				_putchar('_');
+			}
+			_putchar('\\');
+			_putchar('n');
+			line++;
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
