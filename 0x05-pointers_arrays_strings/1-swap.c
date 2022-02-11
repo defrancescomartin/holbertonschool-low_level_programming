37061d9e1fe4cfd4a032d790586dc7ece6a6515b
MartinDeFrancesco <defrancescoma@gmail.com>
2022-02-11 08:56:13 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - takes a pointer and updates the value it point to 98
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int aux = *a;
+	*a = *b;
+	*b = aux;
+}
