fe689b629241a1c89c94462fa43bc23d8572d440
MartinDeFrancesco <defrancescoma@gmail.com>
2022-02-11 09:52:50 -0800

    rev string 2.0

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse followed
+ * rev_string - prints a string in reverse followed
  * by a new line
  * @s: string
  * Return: void
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int i = 0;
 
