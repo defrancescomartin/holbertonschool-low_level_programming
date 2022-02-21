338b27b2c066f6e2882c6b17547483eedaea9fd7
MartinDeFrancesco <defrancescoma@gmail.com>
2022-02-21 09:31:02 -0800

    re try w/ for

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,21 +11,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	int j;
+        int i, j;
 
-	while (*(s + i) != '\0')
-	{
-		j = 0;
-		while (*(accept + j) != '\0')
-		{
-			if (*(s + i) == *(accept + j))
-			{
-				return (s + i);
-			}
-			j++;
-		}
-		i++;
-	}
-	return (NULL);
+        for (i = 0; *(s + i) != '\0'; i++)
+        {
+                for (j = 0; *(accept + j) != '\0'; j++)
+                {
+                        if (*(s + i) == *(accept + j))
+                        {
+                                return (s + i);
+                        }
+                }
+        }
+        return (NULL);
 }
