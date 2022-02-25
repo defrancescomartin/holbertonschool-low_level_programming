e20ed61e2aaeb30cab68269c836102d290469863
MartinDeFrancesco <defrancescoma@gmail.com>
2022-02-25 07:20:12 -0800

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - adds positive numbers
+ *@argc: argument count
+ *@argv: array of pointers to argument strings
+ *Return: always 0
+**/
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atol(argv[i]); /*en vez de int, a long int por las dudas*/
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
