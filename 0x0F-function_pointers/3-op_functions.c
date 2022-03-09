f57fe5def0fff4c65d7fd5198d275cf4b3f684c4
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-09 14:07:15 -0800

    final version of 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -57,7 +57,7 @@ int op_mul(int a, int b)
  * Return: division of a by b
  */
 
-int op_div (int a, int b)
+int op_div(int a, int b)
 {
 	return (a / b);
 }
@@ -71,7 +71,7 @@ int op_div (int a, int b)
  * Return: the remainder of the division of a by b
  */
 
-int op_mod (int a, int b)
+int op_mod(int a, int b)
 {
 	return (a % b);
 }
