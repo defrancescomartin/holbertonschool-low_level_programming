b82123fd0e762e58d8969cd670959594a0adc726
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-09 14:04:49 -0800

    final version of 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,15 +7,13 @@
  * @argc: number of args
  * @argv: array of args
  *
- * Return: Always 0
+ * Return: 0 if success, 98 99 or 100 if error
  */
 
 int main(int argc, char *argv[])
 {
-	char *op(int, int);
-	int a;
-	int b;
-	int tot;
+	int (*op)(int, int);
+	int a, b;
 
 	if (argc != 4)
 	{
@@ -24,21 +22,21 @@ int main(int argc, char *argv[])
 	}
 
 	a = atoi(argv[1]);
+	op = get_op_func(argv[2]);
 	b = atoi(argv[3]);
-	op = get_op_func (argv[2]);
 
-	if (!op)
+	if (op == NULL || (argv[2][1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", op(a, b));
 	return (0);
 }
