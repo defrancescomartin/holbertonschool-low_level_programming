1a240dd2cf82f400821413007debc84281b2138f
defrancescomartin <defrancescoma@gmail.com>
2022-03-10 20:00:05 +0000

    pintall prototypes done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * print_ - prints a 
- * @: 
+ * print_c - prints a
+ * @c: char to print
  *
  * Return: void
  */
@@ -13,3 +13,66 @@ void print_c(va_list c)
 {
 	printf("%c", va_arg(c, int));
 }
+
+/**
+ * print_i - prints an int
+ * @i: int to print
+ *
+ * Return: void
+ */
+
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int);
+}
+
+/**
+ * print_f - prints a float
+ * @f: float to print
+ *
+ * Return: void
+ */
+
+void print_f(va_list f)
+{
+	printf("%d", va_arg(f, double));
+}
+
+/**
+ * print_s - prints a string
+ * @s: string to print
+ *
+ * Return: void
+ */
+
+void print_s(va_list s)
+{
+	char *str = va_arg(str, char*);
+	if (str == NULL)
+		printf("%s", "(nil)");
+	printf("%s", str);
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
+		{NULL, NULL}
+	};
+
+	va_list
+
+	va_start
+
+	va_end
+	printf("\n");
