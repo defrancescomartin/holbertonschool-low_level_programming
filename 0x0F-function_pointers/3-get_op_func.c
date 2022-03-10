c0c455dab5924354272180b41f93201bb9497ba3
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-09 18:55:35 -0800

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,13 +22,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 
 	int i = 0;
-
 	while (i < 5)
 	{
-		if (*s == *ops[i].op)
+		if (ops[i].op[0] == s[0])
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
-
 	return (NULL);
 }
