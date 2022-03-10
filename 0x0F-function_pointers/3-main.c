43838391ad5d062380413b53121a457d6d4ba712
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-09 18:51:00 -0800

    final fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,21 +21,22 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	a = atoi(argv[1]);
-	op = get_op_func(argv[2]);
-	b = atoi(argv[3]);
-
-	if (op == NULL || (argv[2][1] != '\0'))
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == 0)
+	op = get_op_func(argv[2]);
+
+	if (op == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	printf("%d\n", op(a, b));
 	return (0);
