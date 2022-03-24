c151d2e45370c949348f85ee3981f646ff7594e6
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-24 08:11:16 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - converts a binary num to an u int
- * @b: pointer to a string of 0 and 1 chars 
+ * @b: pointer to a string of 0 and 1 chars
  * Return: the converted num or 0 if error
  */
 
@@ -13,13 +13,13 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 
 	if (b == NULL)
-	return 0;
+	return (0);
 
 	while (b[i] == '0' || b[i] == '1')
 	{
 		val <<= 1;
-		val += b[i]-'0';
+		val += b[i] - '0';
 		i++;
 	}
-	return val;
+	return (val);
 }
