8020854bab62944028b36cee5fc630515659f207
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-28 10:42:10 -0700

    text fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_context == NULL)
+	if (text_content == NULL)
 		return (1);
-	writefile = write(fd, text_context, strlen(text_context));
+	writefile = write(fd, text_content, strlen(text_content));
 	close(fd);
 	if (writefile == -1)
 	{
