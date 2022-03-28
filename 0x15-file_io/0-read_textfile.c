807485c9450d7f4630d687a2be2af21d287f4514
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-28 09:39:20 -0700

    read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, size,
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	fd = open (filename, O_RDONLY);
+	size = write(STDOUT_FILENO, buff, read(fd, buff, letters));
+	if (fd == -1 || size == -1)
+		return (0);
+
+	close(fd);
+	free(buff);
+	return (size);
+}
