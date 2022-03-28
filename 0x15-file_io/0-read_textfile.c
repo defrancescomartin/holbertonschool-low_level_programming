fee830584959f13141a4fcdeb8fa92431d2922c1
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-28 09:44:21 -0700

    fixing betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buff == NULL)
 		return (0);
 
-	fd = open (filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	size = write(STDOUT_FILENO, buff, read(fd, buff, letters));
 	if (fd == -1 || size == -1)
 		return (0);
