1357e5bbcc822e7cc6fbf41d52e08c4c8a8e34aa
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-28 17:09:10 -0700

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,11 +9,12 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, writefile, i;
+
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
- 
+
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
