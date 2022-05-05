f14b6ff25b8a1b68df96d0ee8b0c78a1c1db4042
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-05 09:50:27 -0700

    get node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
 /**
  * get_dnodeint_at_index - gets node at especificated index
  * @head: head of the double linked list
- * @i: index to the node
+ * @index: index to the node
  * Return: the specific node
  */
 
