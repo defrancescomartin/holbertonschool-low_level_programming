c1927cd0e5df4d4756b0c7f89adeefbb84193904
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-26 11:50:42 -0700

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_print - prints a hash table
+  * @ht: hash table to prints
+  * Return: void
+  */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *elem;
+	int flag = 1;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+
+	for (index = 0; index < ht->size; index++)
+	{
+		for (elem = ht->array[index]; elem != NULL; elem = elem->next)
+		{
+			if (flag)
+			{
+				printf("'%s': '%s'", elem->key, elem->value);
+				flag = 0;
+			}
+
+			else
+				printf(", '%s': '%s'", elem->key, elem->value);
+		}
+
+	}
+	printf("}\n");
+}
